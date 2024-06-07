8fcd99fb8ea01dac2276e7dd1466cb188d0009df
tractorjuice <129532814+tractorjuice@users.noreply.github.com>
2024-06-07 08:28:42 +0000

    Ran it through checks

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,8 @@
-import os, re, base64
+import os
+import base64
 import streamlit as st
-from github import Github
-from wardley_map import create_wardley_map_plot, get_owm_map, parse_wardley_map, create_svg_map
+from github import Github, GithubException
+from wardley_map import create_wardley_map_plot, get_owm_map, create_svg_map
 
 API_ENDPOINT = "https://api.onlinewardleymaps.com/v1/maps/fetch?id="
 GITHUB = st.secrets["GITHUB"]
@@ -19,7 +20,6 @@ map_dict = {
     "Fixed Penalty Notices": "gTTfD4r2mORudVFKge",
 }
 
-
 # Reset the map on page reload
 def reset_map():
     st.session_state["messages"] = []
@@ -29,8 +29,7 @@ def reset_map():
     st.session_state["generated"] = []
     st.session_state["disabled_buttons"] = []
 
-
-st.set_page_config(page_title="Chat with your  Map", layout="wide")
+st.set_page_config(page_title="Chat with your Map", layout="wide")
 
 if "map_text" not in st.session_state:
     st.session_state["map_text"] = []
@@ -48,14 +47,13 @@ except GithubException as e:
 map_selection = st.sidebar.radio(
     "Map Selection",
     ("Select from GitHub", "Select from List", "Enter Map ID"),
-    help="Select GitHub to get a list of Simon 's latest research.\n\nSelect from list to get predefined maps.\n\nSelect Enter Map ID to provide your own Onlinewardleymaps id",
+    help="Select GitHub to get a list of Simon's latest research.\n\nSelect from list to get predefined maps.\n\nSelect Enter Map ID to provide your own Onlinewardleymaps id",
     key="map_selection",
 )
 
 if map_selection == "Select from List":
     selected_name = st.sidebar.selectbox("Select Map", list(map_dict.keys()))
     MAP_ID = map_dict[selected_name]
-
 elif map_selection == "Select from GitHub":
     with st.spinner("Fetching latest maps from GitHub"):
         if "file_list" not in st.session_state:
@@ -67,7 +65,6 @@ elif map_selection == "Select from GitHub":
                     contents.extend(REPO.get_contents(file_item.path))
                 else:
                     file_name = file_item.name
-                    # Check if the file name starts with a '.', has no extension, or is named 'LICENSE'
                     if (
                         not file_name.startswith(".")
                         and os.path.splitext(file_name)[1] == ""
@@ -88,7 +85,6 @@ else:
 if map_selection != "Select from GitHub":
     if st.session_state.get("current_map_id") != MAP_ID:
         reset_map()
-        del st.session_state["messages"]
         st.session_state["current_map_id"] = MAP_ID
         st.session_state["map_text"] = get_owm_map(MAP_ID)
 
@@ -102,18 +98,18 @@ if map_selection == "Select from GitHub":
 if "map_text" in st.session_state:
 
     # Get the Wardley Map
-    map, map_plot = create_wardley_map_plot(st.session_state.map_text)
+    map, map_plot = create_wardley_map_plot(st.session_state["map_text"])
     svg_map = create_svg_map(map_plot)
-    
+
     # Encode as base 64
     svg_b64 = base64.b64encode(svg_map.encode("utf-8")).decode("utf-8")
-    
+
     # Create CSS wrapper
     css = '<p style="text-align:center; display: flex; justify-content: {};">'.format("center")
-    
+
     # Create HTML
     html_map = r'{}<img src="data:image/svg+xml;base64,{}"/>'.format(css, svg_b64)
-    
+
     # Write the HTML
     st.write(html_map, unsafe_allow_html=True)
 
@@ -128,6 +124,6 @@ if "map_text" in st.session_state:
 
         # Display any warnings drawing the map
         if map.warnings:
-            st.write("Warnings parsing and the drawing map")
+            st.write("Warnings parsing and drawing the map")
             for map_message in map.warnings:
                 st.warning(map_message)
